a476743c87e6bd39591d193e6149d131c828ff3a
Abhaydav <ab4ayadav@gmail.com>
2024-07-27 19:27:59 +0000

    container config done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all JS code as a newer version
+
+//alert(3+3)  we are using nodejs not browser here it shoud be written like this
+
+//code readability should be high
+console.log(3 + 3)
+
+let name ="Abhay"
+let age =18
+let isLoggedIn=false
+
+//number =>2 to power 53
+//bigint
+//string => " "
+//boolean =>true/false
+//null => standalone value
+// undefined =>undefined
+//symbol => unique
+//object
+
+
+console.log(typeof "Abhay")//string
+console.log(typeof null)//object
+console.log(typeof undefined)//undefined
