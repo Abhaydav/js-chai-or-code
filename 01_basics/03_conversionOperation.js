23c9a545f809f8b70c22ea56defa2a14fef5d8a7
Abhaydav <ab4ayadav@gmail.com>
2024-07-27 19:50:20 +0000

    container config done

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,12 @@
+let score="33abc"
+
+console.log(typeof score)//string
+
+let valueInNumber=Number(score)//changing type string to number 
+console.log(typeof valueInNumber) //output => number
+
+console.log(valueInNumber)//NaN means not a number
+
+// "33" => 33
+//"33abc" => NaN
+//true =>1 ,false =>0
