567cc2bca79fff0af98c825af7b72ab9daeea4b1
Abhaydav <ab4ayadav@gmail.com>
2024-07-27 20:38:53 +0000

    container config done

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,7 @@
+console.log("2">1);//true
+console.log("02">1);//true  its automaticaly converting string into numbers and comparing them
+
+
+console.log(null>0);//false
+console.log(null==0);//false
+console.log(null>=0);//true
