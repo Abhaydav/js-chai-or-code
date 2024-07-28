7eec8b4e5f6eaa8e2699d31254c1305a719cd155
Abhaydav <ab4ayadav@gmail.com>
2024-07-28 17:52:49 +0000

    container config done

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -10,3 +10,18 @@ const gameName =new String("Abhay")
 console.log(gameName[0]);
 
 console.log(gameName.length);
+console.log(gameName.toUpperCase())
+
+console.log(gameName.charAt(2))
+
+const newString=gameName.substring(0,4);
+console.log(newString);
+
+const anotherString=gameName.slice(-4,4)
+console.log(anotherString)
+
+//to remove unwanted spaces
+const newStringOne="    Hitesh   "
+console.log(newStringOne) //its having space 
+//to remove spaces we use this
+console.log(newStringOne.trim()) //spaces removed
