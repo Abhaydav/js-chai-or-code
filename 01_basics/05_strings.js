cf5d6548a839f7b156ca5d3db3fae5a692c467bb
Abhaydav <ab4ayadav@gmail.com>
2024-07-28 17:27:10 +0000

    container config done

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,12 @@
+const name="Abhay"
+const repoCount=50
+
+//console.log(name + repoCount + "value")  this should not be used because old fashioned
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);//this `` should be used
+
+const gameName =new String("Abhay")
+
+console.log(gameName[0]);
+
+console.log(gameName.length);
