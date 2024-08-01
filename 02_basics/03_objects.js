2a423068c517660e59519a97a2cddcd956a723ad
Abhaydav <ab4ayadav@gmail.com>
2024-08-01 14:32:15 +0000

    container config done

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,30 @@
+//singletons
+//Object.create
+
+//object literals
+
+//symbol
+const mySym=Symbol("key1")
+const jsUser={
+    name:"hitesh",
+    [mySym]:"myKey1",
+    age:18,
+    location:"jaipur",
+    "email":"hitesh@google.com",
+    isLoggedIn:false,
+    lastLoginDays:["monday","saturday"]
+
+}
+console.log(jsUser.name)
+console.log(jsUser["email"])//this can also be used
+
+console.log(jsUser[mySym])//this is used to call symbol value
+
+jsUser.email="hitesh@chatgpt.com"//to modify in object
+console.log(jsUser.email)
+//to stop modification in object e use freeze
+Object.freeze(jsUser)
+jsUser.email="hitesh@microsoft.com"
+console.log(jsUser.email)
+
+
