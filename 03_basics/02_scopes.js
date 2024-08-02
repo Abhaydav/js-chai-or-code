847254525a95414c27807c5629464fc3018ed333
Abhaydav <ab4ayadav@gmail.com>
2024-08-02 19:35:03 +0000

    container config done

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -64,7 +64,7 @@ function addone(num){
     return num+1
 }
 
-//console.log(addTwo(5)) //here we cant its called hoisting
+//console.log(addTwo(5)) //here we cant its called hoisting 
 const addTwo=function (num){
     return num+2
 }
