843cd836310acac952b7ca209c8122bbacd96cba
Abhaydav <ab4ayadav@gmail.com>
2024-08-05 19:12:36 +0000

    container config done

---
+++ 05_iterations/seven.js
@@ -0,0 +1,14 @@
+//here we will use map
+
+const myNumbers=[1,2,3,4,5,6,7,8,9,10]
+
+// const newNumbers=myNumbers.map ((num)=> num+=10)
+// console.log(newNumbers);
+
+// const newNumbers=myNumbers.map((num)=> num*10)
+// console.log(newNumbers);
+
+
+const newNumbers=myNumbers.map((num)=> num*10).map((num)=>num+1).filter((num)=> num>=40)
+console.log(newNumbers);
+
