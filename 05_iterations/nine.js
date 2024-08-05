597c76e6621bd14c3632ee2e16ceebae6cd1e776
Abhaydav <ab4ayadav@gmail.com>
2024-08-05 19:58:46 +0000

    container config done

--- 05_iterations/nine.js
+++ 05_iterations/nine.js
@@ -5,9 +5,32 @@ const myNums=[1,2,3]
 // const myTotal=myNums.reduce((acc,curr)=> (acc+curr),0)
 // console.log(myTotal);
 
-const myTotal=myNums.reduce(function(acc,curr){
-    console.log(`acc: ${acc} curr:${curr}`);
-    return acc+curr
-},0)
+// const myTotal=myNums.reduce(function(acc,curr){
+//     console.log(`acc: ${acc} curr:${curr}`);
+//     return acc+curr
+// },0)
 
-console.log(myTotal);
+// console.log(myTotal);
+
+//here reduce used perfectly
+const shoppingCart=[
+    {
+        itemName:"js course",
+        price:2999
+    },
+    {
+        itemName:"py course",
+        price:5999
+    },
+    {
+        itemName:"cpp course",
+        price:3999
+    },
+    {
+        itemName:"java course",
+        price:9999
+    }
+]
+
+const cartTotal=shoppingCart.reduce((acc,curr)=> acc+curr.price,0)
+console.log(cartTotal);
