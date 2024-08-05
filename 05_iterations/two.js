7f925e5dd56ea6cbe72314c50cbec4f51cc23288
Abhaydav <ab4ayadav@gmail.com>
2024-08-05 16:02:10 +0000

    container config done

---
+++ 05_iterations/two.js
@@ -0,0 +1,13 @@
+
+// const index = 0
+// while (index <=10){
+//     console.log(`value of index is ${index}`);
+//     index >10;
+// }
+
+let myArray=['flash','batman','superman']
+let arr=0
+while (arr <myArray.length) {
+    console.log(`Value is${myArray[arr]}`);
+    arr=arr + 1
+}
