1c69125b359a29f075c36ddf38c0d6bbbcfc8472
Abhaydav <ab4ayadav@gmail.com>
2024-08-05 18:42:34 +0000

    container config done

---
+++ 05_iterations/six.js
@@ -0,0 +1,14 @@
+const books=[
+    {title:"book one",genre:"Fiction",publish:"1981",edition:"2004"},
+    {title:"book two",genre:"Non-Fiction",publish:"1992",edition:"2008"},
+    {title:"book three",genre:"History",publish:"1999",edition:"2007"},
+    {title:"book four",genre:"Science",publish:"1989",edition:"2010"},
+    {title:"book five",genre:"History",publish:"1981",edition:"2004"},
+    {title:"book six",genre:"Science",publish:"1989",edition:"2008"}
+
+]
+// const userBooks=books.filter((bk)=> bk.genre==="History")
+// console.log(userBooks);
+
+const userBooks=books.filter((bk)=> bk.genre==="History" && bk.publish==="1981")
+console.log(userBooks);
