edb6fa2c239a636ec807d494278167797c7cfbdd
Abhaydav <ab4ayadav@gmail.com>
2024-08-06 18:39:56 +0000

    container config done

---
+++ 06_dom/domselectors.js
@@ -0,0 +1,17 @@
+//these are used on browser
+//querySelector
+
+document.querySelector('input[type="password"]')
+
+document.querySelector('ul')//to select a unordered list
+
+const myul=document.querySelector('ul')
+myul.querySelector('li')
+
+
+//querrySelectorAll
+
+document.querySelectorAll('li')
+const tempLiList=document.querySelectorAll('li')
+tempLiList
+templist[0].style.color='green'
